cb812e6b92f5f81307a9a4fec159f0b2be795a45
Maemolee <iblueer@live.cn>
2018-01-07 20:49:34 +0800

    识别特殊符号
    现在句子里出现中文的，。？！：；…—（）【】「」『』、《》等等符号的时候，就不会和英文之间添加空格了

--- addingSpaceBetweenChineseAndEnglish/main.m
+++ addingSpaceBetweenChineseAndEnglish/main.m
@@ -10,6 +10,10 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        // 提前设定好中文特殊符号，不加空格的
+        // 这些特殊字符都不会添加空格。
+        NSArray *chineseSymbols = @[@"，", @"。", @"？", @"！", @"：", @"；", @"、", @"（", @"）", @"【", @"】", @"「", @"」", @"", @"『", @"』", @"《", @"》", @"…", @"—"];
+        
         // 读取md文件到NSMutableString
         NSError *error;
         NSMutableString *stringWithFile= [[NSString alloc] initWithContentsOfFile:@"/tmp/test.md"
@@ -24,52 +28,75 @@ int main(int argc, const char * argv[]) {
         // 查找紧挨着汉字的英文字母
         NSUInteger ending = [stringWithFile length] - 1;
         
-        NSUInteger thisCharacter = 0;
-        NSUInteger nextCharacter = 0;
-        BOOL EAC = thisCharacter == nextCharacter? true : false;
+        // 这两个整形用来标记当前字符和下个字符的类型；
+        // 英文字符=0；中文字=1；中文不需要加空格的符号=2；
+        // 只有中英紧挨着的时候，也就是EAC=1的时候需要加空格。
+        // 至于为什么不用BOOL型，因为我发现BOOL型赋值的时候有BUG，只要第一个是false，后面的他就不看了。
+        // 现在初始化都是默认英文符号
+        NSUInteger thisCharacterIsEnglish = 0;
+        NSUInteger nextCharacterIsEnglish = 0;
+        
+        // 得到EAC
+        // 如果这个字符和下一个字符相同类型，那么EAC就是true，true就不添加空格；
+        // 如果这个字符和下一个字符不同类型，那么EAC就是false，false就添加空格；
+        // BOOL输出的时候是字符类型，%c。
+        BOOL EAC = (thisCharacterIsEnglish + nextCharacterIsEnglish) == 1;
         NSLog(@"defalt EAC: %c", EAC);
         
+        // 建立一个do while循环。
+        // 为什么不用for循环？因为循环的次数会随着程序运行不断增加。
         NSUInteger i = 0;
         do {
             // 输出现在字符串的长度
 //            NSLog(@"ending now is %lu", ending);
             
-            // 得到索引i处的单个字符
+            // 得到索引i处的单个字符x2
             NSRange thisRange = NSMakeRange(i,1);
             NSRange nextRange = NSMakeRange(i+1, 1);
             
+            // 得到字符的单字符子串x2
             NSString *thisSubString = [stringWithFile substringWithRange:thisRange];
             NSString *nextSubString = [stringWithFile substringWithRange:nextRange];
             
-            // 将这个字符转化成C语言字符，判断长度
+            // 将字符转化成C语言字符，判断长度x2
             const char *thisCString=[thisSubString UTF8String];
             const char *nextCString=[nextSubString UTF8String];
             
             // 判断this
             if (strlen(thisCString)==3)
             {
-                NSLog(@"%lu 是汉字", i);
-                thisCharacter = 0;
+                // 如果是中文的限定的符号，则应使它识别为英文（为了不添加空格）
+                if ([chineseSymbols containsObject:thisSubString]) {
+                    NSLog(@"%lu 是汉字特殊符号", i);
+                    thisCharacterIsEnglish = 2;
+                } else {
+                    NSLog(@"%lu 是汉字", i);
+                    thisCharacterIsEnglish = 1;
+                }
             }else if(strlen(thisCString)==1) {
                 NSLog(@"%lu 是字母", i);
-                thisCharacter = 1;
+                thisCharacterIsEnglish = 0;
             }
             
             // 判断next
             if (strlen(nextCString)==3)
-            {
-                NSLog(@"%lu 是汉字", i+1);
-                nextCharacter = 0;
+            {                // 如果是中文的限定的符号，则应使它识别为英文（为了不添加空格）
+                if ([chineseSymbols containsObject:nextSubString]) {
+                    NSLog(@"%lu 是汉字特殊符号", i);
+                    nextCharacterIsEnglish = 2;
+                } else {
+                    NSLog(@"%lu 是汉字", i);
+                    nextCharacterIsEnglish = 1;
+                }
             }else if(strlen(nextCString)==1) {
                 NSLog(@"%lu 是字母", i+1);
-                nextCharacter = 1;
+                nextCharacterIsEnglish = 0;
             }
             
             // 得到EAC
-            // 如果这个字符和下一个字符相同类型，那么EAC就是true，true就不添加空格；
-            // 如果这个字符和下一个字符不同类型，那么EAC就是false，false就添加空格；
-            EAC = thisCharacter == nextCharacter? true : false;
-            if (!EAC) {
+//            EAC = thisCharacterIsEnglish == nextCharacterIsEnglish? true : false;
+            BOOL EAC = (thisCharacterIsEnglish + nextCharacterIsEnglish) == 1;
+            if (EAC) {
                 // 输出要插入空格的位置
                 NSLog(@"Found EAC, at range {location: %lu, length: %lu}", nextRange.location, nextRange.length);
                 
